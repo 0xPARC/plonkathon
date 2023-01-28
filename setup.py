1474550622493f7b80133ca7f36140e8aca6eee2
amirgamil <amir.amgad01@gmail.com>
2023-01-28 12:33:38 -0500

    verification key works!

--- setup.py
+++ setup.py
@@ -80,4 +80,4 @@ class Setup(object):
     # Generate the verification key for this program with the given setup
     def verification_key(self, pk: CommonPreprocessedInput) -> VerificationKey:
         # Create the appropriate VerificationKey object
-        return NotImplemented
+        return VerificationKey(group_order=pk.group_order, Qm=self.commit(pk.QM), Ql=self.commit(pk.QL), Qr=self.commit(pk.QR), Qo=self.commit(pk.QO), Qc=self.commit(pk.QC), S1=self.commit(pk.S1), S2=self.commit(pk.S2), S3=self.commit(pk.S3), X_2=self.X2, w=Scalar.root_of_unity(group_order=pk.group_order))
